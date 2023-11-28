e07c22fdb05172d3759cd3ef9494bb89849220cf
Luka Manitta <luka@lukamanitta.com>
2023-11-28 19:41:43 +1000

    feat: basic definitions for colourscheme and config structures

--- src/colourscheme.rs
+++ src/colourscheme.rs
@@ -1,7 +1,65 @@
-pub struct Colourscheme {
+use colour_utils::Colour;
 
+struct ANSIColours {
+    black: Colour,
+    red: Colour,
+    green: Colour,
+    yellow: Colour,
+    blue: Colour,
+    magenta: Colour,
+    cyan: Colour,
+    white: Colour,
+}
+
+pub struct Colourscheme {
+    background: Colour,
+    foreground: Colour,
+
+    cursor_bg: Colour,
+    cursor_fg: Colour,
+
+    selection_bg: Colour,
+    selection_fg: Colour,
+
+    regular: ANSIColours,
+    bright: ANSIColours,
 }
 
 impl Colourscheme {
+    // TODO: parse colour from toml file
+    pub fn from_toml(toml: &str) -> Colourscheme {
+        Colourscheme{
+            background: Colour::from_rgb(0, 0, 0),
+            foreground: Colour::from_rgb(255, 255, 255),
 
+            cursor_bg: Colour::from_rgb(255, 255, 255),
+            cursor_fg: Colour::from_rgb(0, 0, 0),
+
+            selection_bg: Colour::from_rgb(255, 255, 255),
+            selection_fg: Colour::from_rgb(0, 0, 0),
+
+            regular: ANSIColours {
+                black: Colour::from_rgb(0, 0, 0),
+                red: Colour::from_rgb(255, 0, 0),
+                green: Colour::from_rgb(0, 255, 0),
+                yellow: Colour::from_rgb(255, 255, 0),
+                blue: Colour::from_rgb(0, 0, 255),
+                magenta: Colour::from_rgb(255, 0, 255),
+                cyan: Colour::from_rgb(0, 255, 255),
+                white: Colour::from_rgb(255, 255, 255),
+            },
+
+            bright: ANSIColours {
+                black: Colour::from_rgb(128, 128, 128),
+                red: Colour::from_rgb(255, 0, 0),
+                green: Colour::from_rgb(0, 255, 0),
+                yellow: Colour::from_rgb(255, 255, 0),
+                blue: Colour::from_rgb(0, 0, 255),
+                magenta: Colour::from_rgb(255, 0, 255),
+                cyan: Colour::from_rgb(0, 255, 255),
+                white: Colour::from_rgb(255, 255, 255),
+            },
+        }
+
+    }
 }
--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,20 @@
-pub struct Config {
+use std::path::Path;
 
+pub struct ToolConfig {
+    name: String,
+    template_path: Box<Path>,
+    destination_path: Box<Path>,
+}
+
+pub struct Config {
+    tool_configs: Vec<ToolConfig>,
 }
 
 impl Config {
-
+    // TODO: parse config from toml file
+    pub fn from_toml(toml: &str) -> Config {
+        Config {
+            tool_configs: Vec::new(),
+        }
+    }
 }
