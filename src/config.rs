3c62e9c776eac91f65861e77975b77a0cfa88f46
Luka Manitta <luka@lukamanitta.com>
2024-04-22 18:12:33 +1000

    feat: update config implementation

--- src/config.rs
+++ src/config.rs
@@ -1,20 +1,34 @@
-use std::path::Path;
+use serde::Deserialize;
+use toml::Table;
 
-pub struct ToolConfig {
-    name: String,
-    template_path: Box<Path>,
-    destination_path: Box<Path>,
+extern crate shellexpand;
+
+#[derive(Debug, Deserialize)]
+pub struct ConfigEntry {
+    pub name: String,
+    pub template_path: String,
+    pub destination_path: String,
 }
 
+#[derive(Debug, Deserialize)]
 pub struct Config {
-    tool_configs: Vec<ToolConfig>,
+    pub entries: Vec<ConfigEntry>,
 }
 
 impl Config {
-    // TODO: parse config from toml file
-    pub fn from_toml(toml: &str) -> Config {
-        Config {
-            tool_configs: Vec::new(),
-        }
+    pub fn new(toml_string: &str) -> Config {
+        let mut config = Config { entries: Vec::new() };
+        let config_table = toml_string.parse::<Table>().unwrap();
+        for (key, value) in config_table.iter() {
+            config.entries.push(
+                ConfigEntry {
+                    name: key.to_string(),
+                    template_path: shellexpand::tilde(&value["template"].as_str().unwrap()).to_string(),
+                    destination_path: shellexpand::tilde(&value["destination"].as_str().unwrap()).to_string(),
+                }
+            );
+        };
+
+        return config;
     }
 }
